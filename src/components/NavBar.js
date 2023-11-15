4f141954840167545fd76fc6a7a27923632646d7
Tanaya Naik <tanayanaik23@gmail.com>
2023-11-15 21:37:35 +0530

    updates

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,6 +13,8 @@ export const NavBar = () => {
   const [activeLink, setActiveLink] = useState('home');
   const [scrolled, setScrolled] = useState(false);
 
+
+
   useEffect(() => {
     const onScroll = () => {
       if (window.scrollY > 50) {
